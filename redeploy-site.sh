53febcc2654e903be12768a55e691822d3f387c3
S-Sigdel <sakshyamsigdel@gmail.com>
2026-07-26 23:27:39 +0000

    update redeploy with docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-SESSION="portfolio"
-tmux kill-session -t "$SESSION" 2>/dev/null
-tmux new-session -s "$SESSION" \
-  'cd ~/Portfolio-Sak &&
-git fetch && git reset origin/main --hard &&
-source .venv/bin/activate &&
-pip install -r requirements.txt &&
-systemctl restart myporfolio.service
-exec bash'
+
+set -e
+
+cd ~/Portfolio-Sak
+
+git fetch && git reset origin/main --hard
+
+docker compsose -f docker-compose.prod.yml down 
+
+docker compose -f docker-compose.prod.yml up -d --build
+
